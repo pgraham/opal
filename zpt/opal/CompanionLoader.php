1696bfa0d449a3d7f9acddfd4b7a9f4944f8a565
Philip Graham <philip.robert.graham@gmail.com>
2013-04-30 15:02:59 +0000

    Added support a loader factory and support for retrieving other companions from within a companion

--- zpt/opal/CompanionLoader.php
+++ zpt/opal/CompanionLoader.php
@@ -4,10 +4,10 @@
  * Copyright (c) 2013, Philip Graham
  * All rights reserved.
  *
- * This file is part of php-code-templates and is licensed by the Copyright
- * holder under the 3-clause BSD License.  The full text of the license can be
- * found in the LICENSE.txt file included in the root directory of this
- * distribution or at the link below.
+ * This file is part of O-pal and is licensed by the Copyright holder under the 
+ * 3-clause BSD License.  The full text of the license can be found in the 
+ * LICENSE.txt file included in the root directory of this distribution or at 
+ * the link below.
  * =============================================================================
  *
  * @license http://www.opensource.org/licenses/bsd-license.php
@@ -34,6 +34,9 @@ class CompanionLoader
 
     /* Naming strategy for instantiating instances. */
     private $namingStrategy;
+
+    /* The factory used to create this loader, or a new factory if not set. */
+    private $factory;
 
     /**
      * Create a new loader for classes that live in the given namespace.
@@ -62,9 +65,22 @@ class CompanionLoader
 
         if (!array_key_exists($targetClass, $this->instances)) {
             $instance = $this->instantiate($targetClass);
+            $instance->opalLoader = $this;
             $this->instances[$targetClass] = $instance;
         }
         return $this->instances[$targetClass];
+    }
+
+    /**
+     * Getter for a CompanionLoaderFactory instances.  If this loader was
+     * instantiated by a factory then it will be the factory returned by this
+     * method.
+     *
+     * @return CompanionLoaderFactory
+     */
+    public function getFactory() {
+        $this->ensureFactory();
+        return $this->factory;
     }
 
     /*
@@ -85,6 +101,16 @@ class CompanionLoader
     }
 
     /**
+     * Setter for the factory used to create this loader. This method should 
+     * typically only be invoked by CompanionLoaderFactory instances.
+     *
+     * @param CompanionLoaderFactory $factory
+     */
+    public function setFactory(CompanionLoaderFactory $factory) {
+        $this->factory = $factory;
+    }
+
+    /**
      * Set the naming strategy to use when retrieving aspects.
      *
      * @param NamingStrategy $naminStrategy
@@ -100,15 +126,14 @@ class CompanionLoader
      * =========================================================================
      */
 
-    /* Instantiate an aspect for the specified class. */
-    private function instantiate($targetClass)
-    {
-        $this->ensureNamingStrategy();
-        $className = $this->namingStrategy->getClassName($targetClass);
-        $fq = $this->baseNamespace . "\\$className";
-        $instance = new $fq();
-
-        return $instance;
+    /*
+     * Ensuer that a CompanionLoaderFactory is available. If none has been 
+     * specified then a new factory is instantiated.
+     */
+    private function ensureFactory() {
+        if ($this->factory === null) {
+            $this->factory = new CompanionLoaderFactory();
+        }
     }
 
     /*
@@ -119,5 +144,16 @@ class CompanionLoader
         if ($this->namingStrategy === null) {
             $this->namingStrategy = new DefaultNamingStrategy();
         }
+    }
+
+    /* Instantiate an aspect for the specified class. */
+    private function instantiate($targetClass)
+    {
+        $this->ensureNamingStrategy();
+        $className = $this->namingStrategy->getClassName($targetClass);
+        $fq = $this->baseNamespace . "\\$className";
+        $instance = new $fq();
+
+        return $instance;
     }
 }
