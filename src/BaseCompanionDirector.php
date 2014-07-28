235fc6cb6abde43aa23bd6666417f63f3dbea7a0
Philip Graham <philip.robert.graham@gmail.com>
2014-07-28 03:14:37 +0000

    Added a shell BaseCompanionDirector class for helping to implement the CompanionDirector interface.

---
+++ src/BaseCompanionDirector.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright (c) 2014, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Conductor. For the full copyright and license information
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace zpt\opal;
+
+/**
+ * Base implementation of a {@link CompanionDirector}.
+ *
+ * @author Philip Graham <philip@zeptech.ca>
+ */
+abstract class BaseCompanionDirector implements CompanionDirector
+{
+
+}
